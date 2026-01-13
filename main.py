512925a2fc0b49f172342d9d22e7a4d3354e110f
ChocoRamaBG <127840485+ChocoRamaBG@users.noreply.github.com>
2026-01-13 15:14:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def save_to_excel(data, filename):
 print("Bootleg Chat: Минаваме на директна URL атака в облака...")
 
 # --- ВЪНШЕН ЦИКЪЛ: РЕГИОНИ (02 до 29) ---
-for r in range(29, 30): #Usually it is 2, 30
+for r in range(24, 29): #Usually it is 2, 30
     region_code = f"{r:02d}"
     page_num = 1 
     
@@ -192,6 +192,7 @@ for r in range(29, 30): #Usually it is 2, 30
 save_to_excel(all_data, output_filename)
 driver.quit()
 print(f"Готово, Гащник! Всичко е в {output_filename}.")
+
 
 
 
